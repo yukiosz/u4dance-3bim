773229e5853df921ba07dfb9e0c5b6d9b28f49e3
unknown <rovennavitoria@gmail.com>
2023-10-04 14:30:54 -0300

    adicionando eventos

--- src/main/java/com/danca/U4Dance/Controller/EventoController.java
+++ src/main/java/com/danca/U4Dance/Controller/EventoController.java
@@ -2,29 +2,38 @@ package com.danca.U4Dance.Controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.danca.U4Dance.Database;
 import com.danca.U4Dance.Model.Evento;
+import com.danca.U4Dance.Repository.EventoRepository;
 
 @RestController
+@CrossOrigin
 public class EventoController {
+
+    @Autowired
+    EventoRepository eventoRepository;
+
     @GetMapping("/eventos")
-    public List<Evento> listarEventos(){
-        return Database.recuperaEvento();
+    public List<Evento> recuperar(){
+        return (List<Evento>) eventoRepository.findAll();
     }
 
     @PostMapping("/eventos")
-    public void addEvento(@RequestBody Evento evento){
-        Database.addEvento(evento);
+    public Evento adicionar(@RequestBody Evento evento){
+        return eventoRepository.save(evento);
     }
 
-    @DeleteMapping("/eventos")
-    public void deletarEventos(@RequestBody Evento evento){
-        Database.delEvento(evento);
+    @DeleteMapping("/eventos/{id}")
+    public String deletar(@PathVariable("id") Long id){
+        eventoRepository.deleteById(id);
+        return "O evento de id "+id+" foi excluído!";
     }
 }
---
+++ src/main/java/com/danca/U4Dance/Repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.danca.U4Dance.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.danca.U4Dance.Model.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, Long>{
+    
+}
